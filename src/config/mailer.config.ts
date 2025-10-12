b331617c3af7d09de1c75ce1d5b8a27fb8943472
syni18 <csesai07@gmail.com>
2025-10-12 17:20:20 +0530

    fixes in the smtp configuration to successfull connection establish

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -5,39 +5,29 @@ import logger from 'logger/winston-logger';
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST!,
   port: Number(process.env.SMTP_PORT!),
-  secure: false,
+  secure: Boolean(process.env.SMTP_SECURE!), // true for 465, false for other ports
+  requireTLS: true,
   pool: true,
   maxConnections: 5,
   maxMessages: 100,
   auth: {
     user: process.env.SMTP_USER!,
     pass: process.env.SMTP_PASS!
+  },
+  tls: {
+    rejectUnauthorized: true,
+    minVersion: 'TLSv1.2'
   }
 });
 
 // Verify transporter on startup
 export async function verifyMailerConnection() {
   try {
-    logger.info(`Verifying SMTP transporter... ${JSON.stringify(transporter.options)}`);
     await transporter.verify();
-    logger.info("SMTP transporter verified successfully");
+    logger.info("✅ SMTP transporter verified successfully");
     return true;
   } catch (err) {
-    logger.error("Failed to verify SMTP connection", { error: err });
+    logger.error("❌ Failed to verify SMTP connection", { error: err });
     throw new Error("SMTP connection failed");
   }
 }
-
-async function sendMail() {
-  const info = await transporter.sendMail({
-    from: process.env.SMTP_SENDER!,
-    to: "csesai07@gmail.com",
-    subject: "Test Email from Titan SMTP",
-    text: "Hello from Titan SMTP!",
-    html: "<p>Hello from <b>Titan SMTP</b>!</p>",
-  });
-
-  console.log("Message sent: %s", info.messageId);
-}
-
-sendMail().catch(console.error);
